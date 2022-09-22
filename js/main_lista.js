01338173e23f9be0c89a2621eedbd69eb1b2fed4
Irving Alexis Pascual Hernandez <hpascual117gmail.com>
2022-09-22 16:09:53 -0500

    chore | Se arreglo la sugerencia que se nos solicito para la barra de navegación

--- js/main_lista.js
+++ js/main_lista.js
@@ -52,3 +52,35 @@ const getData = () => {
         console.log("Error en la solicitud" + error)
     });
 }
+let navbar = document.getElementById("nav");
+window.addEventListener("load", function (e) {
+    navbar.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class='container'>
+        <nav class="navbar navbar-light bg-light">
+                    <a class="navbar-brand" href="#">
+            <img src="../src/Logo.png" width="80" class="d-inline-block align-top" alt="">
+          </a>
+        </nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link navBar" href="../Index.html">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link navBar" href="./Conocenos.html">Quienes Somos <span
+                  class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="./Lista_de_items.html">Servicios <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="./contacto.html">Contacto <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+        <ol class="nav-item active">
+          <button type="button" class="btn btn-success">Iniciar Sesión</button>
+        </ol>
+      </div >
+    </nav > `;
+});
