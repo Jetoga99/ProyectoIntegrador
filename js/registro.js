f5970475f53d489b0393bf01cc7476e3974a8220
andres <andrestecpile97@gmail.com>
2022-09-28 08:40:06 -0500

    Chore| Se ha agregado el archivo JS para validación de datos donde validamos los datos de entrada.

--- js/registro.js
+++ js/registro.js
@@ -1,37 +1,215 @@
-var btn = document.getElementById("btn");
-
+var btn = document.getElementById("btn_submit");
+var Email_Warning = document.getElementById("EmailWarn");
 var cont = 0;
+if (localStorage.key(0)) {
+  for (let index = 0; index <= localStorage.key(0); index++) {
+    console.log(localStorage.getItem(localStorage.key(index)));
+    cont = parseInt(localStorage.key(0)) + 1;
+
+  }
+}
+
+
+class user {
+  Name = "";
+  Email = "";
+  Phone = "";
+  User_password = "";
+  static total = cont;
+  id = 0;
+  constructor(Name, Email, Phone, User_password) {
+    this.Name = Name.toUpperCase();
+    this.Email = Email.toLowerCase();
+    this.Phone = Phone;
+    this.User_password = User_password;
+    this.id = user.total;
+    user.total++;
+  }//constructor
+
+}
+
+
+
 
 btn.addEventListener("click", function (e) {
   e.preventDefault();
-  let p1 = document.getElementById("inputPassword1").value;
-  var p2 = document.getElementById("inputPassword2").value;
+  let password1 = document.getElementById("inputPassword1").value;
+  let password2 = document.getElementById("inputPassword2").value;
+  let FullName = document.getElementById("inputName").value;
+  let phoneNumber = document.getElementById("phoneNumber").value;
+  let Email = document.getElementById("inputEmail").value;
 
-  /*while (!espacios && cont < p1.length) {
-      if (p1.charAt(cont) == " ") espacios = true;
-      cont++;
-    }*/
 
-  if (!document.getElementById("inputPassword1").value) {
-    alert("La contraseña no puede contener espacios en blanco");
-    console.log(p1);
-  } else {
-    console.log("false");
-    console.log(p1.length);
+
+  // parte de validacion de datos 
+  if (!ValidateName(FullName)) {  //if de validacion de nombre
+    document.getElementById("nameAlert").style.display = "block";
+    document.getElementById("nameAlert").innerHTML = `<div class="alert alert-danger" role="alert">
+    ¡Ingrese su nombre completo por favor!
+  </div>`;
+    console.log(document.getElementById("inputName").style.borderColor);
+    document.getElementById("inputName").style.borderColor = "#FF0000";
+    setTimeout(() => { }, (10 * 1000)); //me quede aqui mucho ojo
+  }//fin de if validación de nombre 
+
+  // Código de valores por default por el evento change 
+  document.getElementById("inputName").addEventListener("change", function () {
+    document.getElementById("inputName").style.borderColor = "";
+    document.getElementById("nameAlert").style.display = "none";
+  })
+
+
+
+
+  if (!(ValidateNumber(phoneNumber))) {// if de validacion para numero de telefono 
+    document.getElementById("PhoneAlert").style.display = "block";
+    document.getElementById("PhoneAlert").innerHTML = `<div class="alert alert-danger" role="alert">
+    ¡Ingrese su número de teléfono a 10 dígitos sin espacios!
+  </div>`;
+    console.log("por favor ingrese su numero de telefono a 10 digitos");
+    console.log(document.getElementById("phoneNumber").style.borderColor);
+    document.getElementById("phoneNumber").style.borderColor = "#FF0000";
+    usr_number = 0;
+
+
+  }//fin de if validación de número de teléfono.
+  //Código de des alerta por mal ingreso de dato de número de teléfono
+  document.getElementById("phoneNumber").addEventListener("change", function () {
+    document.getElementById("phoneNumber").style.borderColor = "";
+    document.getElementById("PhoneAlert").style.display = "none";
+  })
+
+
+
+
+  if (!ValidateEmail(Email)) {   //validación del correo electrónico 
+    Email_Warning.style.display = "block";
+    Email_Warning.innerHTML = `<div class="alert alert-danger" role="alert">
+    ¡Ingrese un correo electrónico valido ejemplo (nombre@dominio.com)!
+  </div>`;
+    console.log(document.getElementById("inputEmail").style.borderColor);
+    document.getElementById("inputEmail").style.borderColor = "#FF0000";
+
   }
+  document.getElementById("inputEmail").addEventListener("change", function () {
+    document.getElementById("inputEmail").style.borderColor = "";
+    Email_Warning.style.display = "none";
+  })
 
-  if (!document.getElementById("inputPassword2").value) {
-    alert("Favor de confirmar contraseña");
-    console.log(p2.length);
-  } else {
-    console.log("false");
-    console.log(p2.length);
-  }
 
-  if (p1 != p2) {
-    alert("Las passwords deben de coincidir");
-    console.log(p1 + "  " + "vs " + "  " + p2);
-  } else {
-    alert("Todo esta correcto");
+  //validación de contraseñas 
+
+  if (!ValidatePasswords(password1, password2)) {
+    document.getElementById("passwordAlert").style.display = "block";
+    document.getElementById("passwordAlert").innerHTML = `<div class="alert alert-danger" role="alert">
+    Las contraseñas deben coincidir y debe contener al menos 8 caracteres alfanumérico
+  </div>`;
+    document.getElementById("inputPassword1").style.borderColor = "#FF0000";
+    document.getElementById("inputPassword2").style.borderColor = "#FF0000";
+
   }
+  document.getElementById("inputPassword1").addEventListener("change", function () {
+    document.getElementById("inputPassword2").style.borderColor = "";
+    document.getElementById("inputPassword1").style.borderColor = "";
+    document.getElementById("passwordAlert").style.display = "none";
+
+  })
+  document.getElementById("inputPassword2").addEventListener("change", function () {
+    document.getElementById("inputPassword2").style.borderColor = "";
+    document.getElementById("inputPassword1").style.borderColor = "";
+    document.getElementById("passwordAlert").style.display = "none";
+
+  })
+
+  if (((ValidateName(FullName))) && ((ValidateNumber(phoneNumber))) && ((ValidateEmail(Email))) && ((ValidatePasswords(password1, password2)))) {
+    console.log("se ha creado una clasecon nombre : " + FullName);
+    localStorage.setItem(String(cont), JSON.stringify(FullName = new user(FullName, phoneNumber, Email, password1)));
+    cont++;
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: '¡Tus datos han sido guardados exitosamente¡',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    document.getElementById("inputEmail").value = "";
+    document.getElementById("inputName").value = "";
+    document.getElementById("phoneNumber").value = "";
+    document.getElementById("inputPassword2").value = "";
+    document.getElementById("inputPassword1").value = "";
+
+  }
+  console.log(FullName.id, FullName.Name, FullName.Phone, FullName.Email, FullName.User_password);
+
+
+
+
 });
+
+
+function ValidateEmail(usr_email) {
+  var regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+)[.]([a-z]+)(.[a-z]+)?$/;
+  if (usr_email.match(regx)) {
+
+    console.log("valid email ")
+
+    return true;
+  } else {
+    console.log("invalid email ")
+    return false;
+
+  }
+
+}
+
+function ValidateName(name) {
+  let regx = /^([a-zA-Z ]+)([a-zA-Z ])?$/;
+  if ((name.match(regx)) && (name.length >= 3)) {
+    console.log("valid name")
+    return true;
+
+  } else {
+
+    console.log("invalid name ")
+    return false;
+
+  }
+
+}
+
+
+function ValidateNumber(number) {
+
+  let regx = /^([1-9-0]+)([1-9])?$/;
+
+
+  if ((number.match(regx)) && (number.length == 10)) {
+    console.log("valid number")
+
+    return true;
+
+  } else {
+
+    console.log("ivalid number")
+    return false;
+
+  }
+
+};
+
+function ValidatePasswords(password1, password2) {
+
+  var regx = /^([a-zA-Z0-9]+)([a-zA-Z0-9])?$/;
+
+  if ((password1.match(regx)) && (password1.length >= 8) && (password2 === password1) && (password2.match(regx)) && (password2.length >= 8)) {
+    console.log(password1 + " " + password2 + "   COINCIDEN");
+    return true;
+
+  } else {
+
+    console.log(password1 + " " + password2 + " no coinciden");
+    return false;
+
+  }
+
+};
