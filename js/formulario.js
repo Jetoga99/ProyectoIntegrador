704089e30533e15df10b3fba417202338e9bc69a
AldoCristino <aldocristino24@gmail.com>
2022-10-08 21:17:32 -0500

    chore | Se implementó el guardado de los servicios como un arreglo en el localStorage, así como, la lectura del localStorage en la página de servicios para mostrar los servicios agregados | Se implementó una vista previa de la card a agregar a los servicios

--- js/formulario.js
+++ js/formulario.js
@@ -3,39 +3,53 @@ let alerta = document.getElementById("alerta");
 let producto = document.getElementById("producto");
 let descripcion = document.getElementById("descripcion");
 let imagen = document.querySelector("#file");
+let img = " ";
 let cont = 1;
+let listaServicios = [];
+const key = "Serv"
+let prev = document.getElementById("preview");
 
 
 submit_btn.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(imagen.value);
     if (validar(producto)) {//validacion de input producto
+        producto.style.borderColor = "";
         if (validar(descripcion)) {//validacion de input descripcion 
+            descripcion.style.borderColor = "";
             if (imagen.value != 0) {//validacion de imagen
+                imagen.style.borderColor = "";
                 //Crea el objeto servicio con id, nombre y descripcion
-                let servicio = { "id": 0, "name": producto.value, "description": descripcion.value }
+                let servicio = { "id": 0, "name": producto.value, "description": descripcion.value, "Imagen": img }
+                // Agrega el objeto servicio a lista de servicios
+                listaServicios.push(servicio);
                 //comvierte el objeto en json y lo guarda en el localstorage
-                localStorage.setItem("info", JSON.stringify(servicio));
+                console.log(servicio);
+                localStorage.setItem(key, JSON.stringify(listaServicios));
+                //Llama a la funcion preview para mostrar un preview de la card a mostrar
+                prev.innerHTML = preview(servicio.nombre, servicio.description, servicio.Imagen, servicio.id);
                 //cambia el texto de la alerta 
                 //cambia la clase de la alerta de alert-danger a alert-success
                 alerta.innerText = "Servicio agregado";
                 alerta.classList.remove("alert-danger");
                 alerta.classList.add("alert-success");
-                producto.value = " "
-                descripcion.value = ""
-                imagen.value = null
+                //Regresando todos los valores a vacio para nuevo servicio
+                producto.value = " ";
+                descripcion.value = "";
+                imagen.value = null;
+                img = "";
                 const $image = document.querySelector('#image');
                 $image.setAttribute('src', " ");
             } else {
-                alerta.innerText = "Error - No hay imagen"
+                imagen.style.borderColor = "#FF0000";
+                alerta.innerText = "Error - No hay imagen";
             }//else imagen
         } else {
-            alerta.innerText = "Error en la descripción del producto"
+            alerta.innerText = "Error en la descripción del producto";
         }//else descripcion 
     } else {
-        alerta.innerText = "Error en el nombre del producto."
+        alerta.innerText = "Error en el nombre del producto.";
     }//else prodcuto
-
     //muestra de alerta por 5 segundos
     alerta.style.display = "block";
     setTimeout(() => {
@@ -44,7 +58,7 @@ submit_btn.addEventListener("click", function (e) {
 });
 
 
-//alidacion de inputs | Verificando que no esten vacios y que tengan al menos 5 letras
+//validacion de inputs | Verificando que no esten vacios y que tengan al menos 5 letras
 function validar(campo) {
     var regex = /^\s+/
     if (campo.value.length == 0 || campo.value.length < 5) {
@@ -52,6 +66,7 @@ function validar(campo) {
         campo.focus();
         //Agregando clase danger a la alerta
         alerta.classList.add("alert-danger");
+        campo.style.borderColor = "#FF0000";
         return false;
     } else {
         if (campo.value.match(regex)) {
@@ -59,6 +74,8 @@ function validar(campo) {
             campo.focus();
             //Agregando clase danger a la alerta
             alerta.classList.add("alert-danger");
+            campo.style.borderColor = "#FF0000";
+            return false;
         } else {
             return true;
         }
@@ -67,12 +84,14 @@ function validar(campo) {
 }
 
 
-//Imagen agregada a localStorage
+//Imagen agregada
 imagen.addEventListener("change", function () {
     const reader = new FileReader();
     reader.addEventListener("load", () => {
-        //console.log(reader.result);
-        localStorage.setItem("recent-image", reader.result);
+        //Codigo de imagen guardada en variable "img"
+        img = reader.result;
+        //console.log(img);
+        //localStorage.setItem("recent-image", reader.result);
     });
     reader.readAsDataURL(this.files[0]);
 });
@@ -93,3 +112,38 @@ $file.addEventListener('change', (event) => {
     const formData = new FormData($form)
     renderImage(formData)
 })
+
+function preview(nombre, descripcion, image, id) {
+    return `<div  card" style="width: 18rem;hover:">
+    <img class="card-img-top" src="${image}" alt="${nombre}">
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title">${nombre}</h5>
+      <p class="card-text">${descripcion.slice(0, 30)}...</p>
+    
+  <button type="button" class="btn btn-dark mt-auto" data-toggle="modal" data-target="#modal_${id}">
+  Ver más
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="modal_${id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${nombre}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      ${descripcion}
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="window.location.href='../pages/cotiza.html'">Cotiza</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  </div>`
+}
--- js/main_lista.js
+++ js/main_lista.js
@@ -2,6 +2,7 @@ let mainProds = document.getElementById("list-items")
 
 window.addEventListener("load", function () {
   getData();
+  leerStorage();
 });
 
 const getData = () => {
@@ -55,3 +56,43 @@ const getData = () => {
     console.log("Error en la solicitud" + error)
   });
 }
+
+const leerStorage = () => {
+  if (localStorage.length > 0) {
+    let arregloServ = JSON.parse(localStorage.getItem("Serv"));
+    console.log(typeof (arregloServ));
+    arregloServ.forEach(servicio => {
+      mainProds.insertAdjacentHTML("beforeend", `<div  card" style="width: 18rem;hover:">
+        <img class="card-img-top" src="${servicio.Imagen}" alt="${servicio.name}">
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title">${servicio.name}</h5>
+          <p class="card-text">${servicio.description.slice(0, 30)}...</p>
+        
+      <button type="button" class="btn btn-dark mt-auto" data-toggle="modal" data-target="#modal_${servicio.id}">
+      Ver más
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="modal_${servicio.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">${servicio.name}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+          ${servicio.description}
+          </div>
+          <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="window.location.href='../pages/cotiza.html'">Cotiza</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `);
+    })//foreach
+  }//if localStrorage
+}//leerStorage
